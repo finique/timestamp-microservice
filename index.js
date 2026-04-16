b2e755f5c942f1854f91c0e31a57663ae0ca2c3f
finique <alex.fedotenkov@alumni.ie.edu>
2026-04-16 14:37:01 +0200

    timestamp microservice

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/:date', handleDate);
+app.get('/api/', handleDate);
+app.get('/api', handleDate);
+
+function handleDate(req, res) {
+  const date = req.params.date;
+
+  let dateObj;
+
+  if (!date) {
+    dateObj = new Date();
+  } else if (/^\d+$/.test(date)) {
+    dateObj = new Date(parseInt(date));
+  } else {
+    dateObj = new Date(date);
+  }
+
+  if (dateObj.toString() === 'Invalid Date') {
+    return res.json({ error: 'Invalid Date' });
+  }
+
+  res.json({
+    unix: dateObj.getTime(),
+    utc: dateObj.toUTCString()
+  });
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Timestamp microservice listening on port ${port}`);
+});
